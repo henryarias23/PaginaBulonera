a47a3f17af760d1cc9b08c054648622f8aa4f43c
Tannacondia <95078436+Tannacondia@users.noreply.github.com>
2023-11-13 10:44:24 -0300

    Add files via upload

---
+++ js/sweetalert.js
@@ -0,0 +1,59 @@
+Swal.fire({
+	title: "Bienvenidos a BQ GROUP!",
+	 html:'<p class="alerttext">ingrese su email para recibir su promocion</p> <p><input id="email" name="email" placeholder="Email" required/></p> <input type="submit" value="Suscribirse" placeholder="enviar consulta" class="enviar">',
+	// icon:
+	 //confirmButtonText:'',
+	// footer:
+	 width:'50%',
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	timer: 5000,
+	// timerProgressBar:
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	 showConfirmButton: false
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
